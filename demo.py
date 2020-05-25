f7cadcc28df9985c4ab42805a67e1887a4d1893e
hotsunu <gmai8.hdd@gmail.com>
2020-05-25 22:53:06 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,2 +1,17 @@
 print('hello world!')
 
+iNumbers = 12345
+fNumbers = 3.1415
+sString = 'hello'
+bTure = Ture
+lLists = [12345,3.1415,'hello',Ture]
+
+
+for x in range(10):
+  print(x)
+  
+lLists = [[(x+1)*(y+1) for x in range(8)] for y in range(10)]
+
+lLists = [x for x in range(10) if x%3 ==0]
+
+
